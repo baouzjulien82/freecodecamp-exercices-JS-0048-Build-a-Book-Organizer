e87f026c020dc7dfeae76e1a8d09e41295044a60
Julien Baouz <baouzjulien@gmail.com>
2025-10-28 21:43:19 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const books = [
+  {
+    title: "Le parfum",
+    authorName: "Jean Claude Ginger",
+    releaseYear: 1998
+  },
+  {
+    title: "Le Kiwi",
+    authorName: "Christophe Canta",
+    releaseYear: 1800
+  },
+  {
+    title: "Perrier et les bulles",
+    authorName: "Frédéric Castor",
+    releaseYear: 2020
+  },
+];
+
+const sortByYear = (book1, book2) => {
+  
+}
